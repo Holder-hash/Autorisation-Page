f6fccc6a0e5f80a7177fc2b8b32256d130ff1890
h0ler <63481484+Holder-hash@users.noreply.github.com>
2022-12-25 20:21:15 +0300

    second card functional

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ autorisBtn.addEventListener('click', () => {
     width: 48vh;`;
     setTimeout(() => {
         signInBtnSpan.textContent = 'Done';
+        document.querySelector('.log-form ').style = 'opacity: 1;';
     },50)
 })
 
@@ -53,5 +54,33 @@ regPass.onblur = () => {
     }
     else {
         regPassLegend.style = 'margin-top: 9%; color: rgb(74, 41, 134);'
+    }
+}
+
+
+
+//log login legend
+logInp.onfocus = () => {
+    logInpLegend.style = 'margin-top: 2%; color: rgb(105, 8, 129);'
+}
+logInp.onblur = () => {
+    if (logInp.value) {
+        logInpLegend.style = 'margin-top: 2%; color: rgb(105, 8, 129);'
+    }
+    else {
+        logInpLegend.style = 'margin-top: 9%; color: rgb(74, 41, 134);'
+    }
+}
+
+//log password legend
+logPass.onfocus = () => {
+    logPassLegend.style = 'margin-top: 2%; color: rgb(105, 8, 129);'
+}
+logPass.onblur = () => {
+    if (logPass.value) {
+        logPassLegend.style = 'margin-top: 2%; color: rgb(105, 8, 129);'
+    }
+    else {
+        logPassLegend.style = 'margin-top: 9%; color: rgb(74, 41, 134);'
     }
 }
